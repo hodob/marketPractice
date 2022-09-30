679efc13a8bbb864857bfa8ecedd320ecd029dcd
hodob <hodob76@gmail.com>
2022-09-30 15:14:13 +0900

    firstcommit

---
+++ src/main/java/com/hodob/marketPractice/MarketPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.hodob.marketPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketPracticeApplication.class, args);
+	}
+
+}
